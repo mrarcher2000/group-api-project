aa72cc25794cd0b97a494d6a29f292c16597565e
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-03-13 14:38:22 -0700

    updated api script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", bindButtons);
-var apiKey = 
-var publicKey = 
+document.addEventListener("DOMContentLoaded", buttonSearch);
+var apiKey = "68249a1ca55b9f3b6c691fb85d3568c9fb74eede";
+var publicKey = "11a71eed96128ec62bd061b4d096a38a";
 
 function buttonSearch(event) 
 {
@@ -10,18 +10,19 @@ function buttonSearch(event)
     var timestamp = event.timestamp;
     var req = new XMLHttpRequest();
     var marvelWebsite = 'http://gateway.marvel.com/v1/public/characters?name='
-    var heroName = document.getElementById.('heroName').value;
+    var heroName = document.getElementById('heroName').value;
     var web = marvelWebsite + heroName + "ts=" + timestamp + 'apiKey=' + apiKey + '$hash=' + publicKey;
 
         document.getElementById('submit').addEventListener('click', function (event) {
-
+            document.getElementById('heroName').textContent = "";
+            document.getElementById('description').textContent = "";
         });
 
         req.open('GET', web, true);
 
-        req.setRequestHeader('Content-Tyoe', 'application/json');
+        req.setRequestHeader('Content-Type', 'application/json');
         req.addEventListener('load', function() {
-            if(req.status >=200 && req.status <400) {
+            if(req.status >= 200 && req.status < 400) {
                 var result = JSON.parse(req.responseText);
                 document.getElementById('name').textContent = result.name;
                 document.getElementById('description').texContent = result.description;
